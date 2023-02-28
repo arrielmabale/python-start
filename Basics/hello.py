d918a5ef4e422c03dfed094bdb8b43fd72d99993
Arriel Mabale <arriel@haditi.com>
2023-02-28 12:55:26 +0400

    basic

--- Basics/hello.py
+++ Basics/hello.py
@@ -1,8 +1,9 @@
 
 print('Hello, World!')
 print('What is your name?')
+myName = 'Arriel'
 myName = input()
-print(type(myName))
+#print(type(myName))
 #print('Nice to meet you ' + myName)
 print('The lenght of yout name is : ')
 print(len(myName))
