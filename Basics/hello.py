dc1c645870eccf6dfbb09d2c011cb29f621eb14e
Arriel Mabale <arriel@haditi.com>
2023-02-28 10:36:07 +0400

    start

---
+++ Basics/hello.py
@@ -0,0 +1,8 @@
+
+print('Hello, World!')
+print('What is your name?')
+myName = input()
+print('Nice to meet you ' + myName)
+print('The lenght of yout name is : ')
+print(len(myName))
+print('What is your age?')
