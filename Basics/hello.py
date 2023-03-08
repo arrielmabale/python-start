966ffbc7b06a3f101ee4014e315ab24b78ff5c41
Arriel Mabale <arriel@haditi.com>
2023-03-08 13:54:55 +0400

    test

--- Basics/hello.py
+++ Basics/hello.py
@@ -1,10 +1,45 @@
 
+myName = 'Arriel'
+myAge = 40
+youngerBrotherAge = 20
+youngerBrotherName = 'Boy'
+
+
 print('Hello, World!')
 print('What is your name?')
-myName = 'Arriel'
-myName = input()
-#print(type(myName))
-#print('Nice to meet you ' + myName)
+
+myInputName = str(input())
+
+if myInputName!='' :
+    print('Nice to meet you ' + myInputName)
+else:
+    print('You seem to be quiet, I am ' + myName)
+
 print('The lenght of yout name is : ')
-print(len(myName))
+print(len(myInputName))
 print('What is your age?')
+
+myInputAge =  input()
+try:
+    myInputAge = int(myInputAge)
+except ValueError:
+    myInputAge = 0
+
+print(type(myInputAge))
+
+if type(myInputAge) !=  int :
+    myInputAge =0
+    
+
+if myInputAge > 0 :
+    print('Your age is ' + str(myInputAge))
+    if myInputAge == youngerBrotherAge:
+        print('Your the same age as my ' + youngerBrotherName + '!')
+    elif myInputAge > youngerBrotherAge: 
+        print('You are older than ' + youngerBrotherName)
+    else :
+        print('you are younger than ' + youngerBrotherName)
+else: 
+    print('Wow your immortal, my age is ' + str(myAge))
+
+exit()
